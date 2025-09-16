77964d965d58ef1e6630c2a5b15cb7b22c350b52
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-09-16 15:28:56 -0500

    Add comments

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const submitButton = document.getElementById("submit");
-const nameInput = document.getElementById("name");
-const dateInput = document.getElementById("date");
-const emailInput = document.getElementById("email");
-const websiteInput = document.getElementById("website");
-const phoneInput = document.getElementById("phone");
-const form = document.getElementById("form");
+const submitButton = document.getElementById("submit"); //submit button
+const nameInput = document.getElementById("name"); //name input
+const dateInput = document.getElementById("date"); //date input
+const emailInput = document.getElementById("email"); //email input
+const websiteInput = document.getElementById("website"); //website input
+const phoneInput = document.getElementById("phone"); //phone input
+const form = document.getElementById("form"); //form
 submitButton.addEventListener("click", (e) => {
-    e.preventDefault();
+    //when submit button is clicked
+    e.preventDefault(); //prevent form from submitting
     if (form.checkValidity()) {
+        //if form is valid
         alert("All inputs are valid");
     } else {
+        //if form is invalid
         const invalidFields = Array.from(
             form.querySelectorAll(".was-validated .form-control:invalid"),
-        );
+        ); //get list of invalid fields
         const invalidFieldsText = invalidFields
             .map((field) => field.id)
             .join(", ");
