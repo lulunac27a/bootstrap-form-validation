a88acd777c8924fc4143bcddbc8ecb94b957d1bb
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-09-16 15:19:57 -0500

    Add JavaScript code

---
+++ index.js
@@ -0,0 +1,17 @@
+const submitButton = document.getElementById('submit');
+const nameInput = document.getElementById('name');
+const dateInput = document.getElementById('date');
+const emailInput = document.getElementById('email');
+const websiteInput = document.getElementById('website');
+const phoneInput = document.getElementById('phone');
+const form = document.getElementById('form');
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (form.checkValidity()) {
+        alert('All inputs are valid');
+    } else {
+        const invalidFields = Array.from(form.querySelectorAll('.was-validated .form-control:invalid'));
+        const invalidFieldsText = invalidFields.map(field => field.id).join(', ');
+        alert(`The following fields are invalid: ${invalidFieldsText}`);
+    }
+})
